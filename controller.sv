40759a2c8570b116cb436ce650dbc7f233efcb2c
Fatim-Sohail <fatimso357@gmail.com>
2023-12-10 23:14:04 +0500

    csr updated

--- controller.sv
+++ controller.sv
@@ -5,15 +5,18 @@ module controller
     input  logic [2:0] funct3,
     input  logic       br_taken, // form br_cond block
     output logic [3:0] aluop,
-    output logic       rf_en,  // control signal for write operation in register file
-    output logic       sel_a,  // control signal to opr_a select MUX to ALU
-    output logic       sel_b,  // control signal to opr_b select MUX to ALU
-    output logic       rd_en,  // contorl signal for reading from data memory
-    output logic       wr_en,  // control signal for writing into data memory
-    output logic [1:0] wb_sel, // control signal for writeback MUX
+    output logic       rf_en,    // control signal for write operation in register file
+    output logic       sel_a,    // control signal to opr_a select MUX to ALU
+    output logic       sel_b,    // control signal to opr_b select MUX to ALU
+    output logic       rd_en,    // contorl signal for reading from data memory
+    output logic       wr_en,    // control signal for writing into data memory
+    output logic [1:0] wb_sel,   // control signal for writeback MUX
     output logic [2:0] mem_acc_mode,
-    output logic [2:0] br_type, // goes to branch cond block to tell which comparison to perform
-    output logic       br_take // to program counter MUX
+    output logic [2:0] br_type,  // goes to branch cond block to tell which comparison to perform
+    output logic       br_take,  // to program counter MUX
+    output logic       csr_rd,   // control signal to read from csr register file
+    output logic       csr_wr,   // control signal to write to csr register
+    output logic       is_mret   // control signal for 'mret' instruction --- 1 will indicate that the inst is mret
 );
     always_comb
     begin
@@ -29,6 +32,9 @@ module controller
                 br_take      = 1'b0;
                 mem_acc_mode = 3'b111;
                 br_type      = 3'b111;
+                csr_rd       = 1'b0;
+                csr_wr       = 1'b0;
+                is_mret      = 1'b0;
                 case(funct3)
                     3'b000: 
                     begin
@@ -63,6 +69,9 @@ module controller
                 br_take      = 1'b0;
                 mem_acc_mode = 3'b111;
                 br_type      = 3'b111;
+                csr_rd       = 1'b0;
+                csr_wr       = 1'b0;
+                is_mret      = 1'b0;
                 case (funct3)
                     3'b000: aluop = 4'b0000; //ADDI
                     3'b010: aluop = 4'b0011; //SLTI
@@ -82,15 +91,18 @@ module controller
             end
             7'b0000011: // I-type - Load Instructions
             begin
-                rf_en    = 1'b1;
-                sel_a    = 1'b1;
-                sel_b    = 1'b1;
-                rd_en    = 1'b1;
-                wb_sel   = 2'b10;
-                wr_en    = 1'b0;
-                br_take  = 1'b0;
-                aluop    = 4'b0000; // aluop is always addition in case of load instructions
-                br_type  = 3'b111;
+                rf_en     = 1'b1;
+                sel_a     = 1'b1;
+                sel_b     = 1'b1;
+                rd_en     = 1'b1;
+                wb_sel    = 2'b10;
+                wr_en     = 1'b0;
+                br_take   = 1'b0;
+                aluop     = 4'b0000; // aluop is always addition in case of load instructions
+                br_type   = 3'b111;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
                 case(funct3)
                     3'b000: mem_acc_mode = 3'b000; // Byte access
                     3'b001: mem_acc_mode = 3'b001; // Halfword access
@@ -98,40 +110,21 @@ module controller
                     3'b100: mem_acc_mode = 3'b011; // Byte unsigned access
                     3'b101: mem_acc_mode = 3'b100; // Halfword unsigned access
                 endcase
-                case (funct3)
-                    3'b011:
-                        // CSRRW instruction
-                        CSRReadEnable = 1'b1;
-                        CSRWriteEnable = 1'b1;
-                    default:
-                        // Handle other I-type instructions
-                        CSRReadEnable = 1'b0;
-                        CSRWriteEnable = 1'b0;
-                endcase
-                case (funct3)
-                    3'b110:
-                        // MRET instruction
-                        CSRReadEnable = 1'b1;
-                        CSRWriteEnable = 1'b1;
-                        interrupt = 1'b0; // Disable interrupt
-                    default:
-                        // Handle other I-type instructions
-                        CSRReadEnable = 1'b0;
-                        CSRWriteEnable = 1'b0;
-                        interrupt = 1'b0; // Default: No interrupt
-                endcase
             end
             7'b0100011: // S-type - Store Instructions
             begin
-                rf_en    = 1'b0;
-                sel_a    = 1'b1;
-                sel_b    = 1'b1;
-                rd_en    = 1'b0;
-                wb_sel   = 2'b01;  // in this case it is don't care because rf_en = 1'b0
-                wr_en    = 1'b1;
-                br_take  = 1'b0;
-                aluop    = 4'b0000; // aluop is always addition in case of store instructions
-                br_type  = 3'b111;
+                rf_en     = 1'b0;
+                sel_a     = 1'b1;
+                sel_b     = 1'b1;
+                rd_en     = 1'b0;
+                wb_sel    = 2'b01;  // in this case it is don't care because rf_en = 1'b0
+                wr_en     = 1'b1;
+                br_take   = 1'b0;
+                aluop     = 4'b0000; // aluop is always addition in case of store instructions
+                br_type   = 3'b111;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
                 case(funct3)
                     3'b000: mem_acc_mode = 3'b000; // Byte access
                     3'b001: mem_acc_mode = 3'b001; // Halfword access
@@ -140,63 +133,114 @@ module controller
             end
             7'b1100011: // B-type
             begin
-                rf_en    = 1'b0;
-                sel_a    = 1'b0;
-                sel_b    = 1'b1;
-                rd_en    = 1'b0;
-                wb_sel   = 2'b01; // in this case it is don't care because rf_en = 1'b0
-                wr_en    = 1'b0;
-                aluop    = 4'b0000; // aluop is always addition in case of branch instructions
-                br_type  = funct3;
-                br_take = br_taken;
+                rf_en     = 1'b0;
+                sel_a     = 1'b0;
+                sel_b     = 1'b1;
+                rd_en     = 1'b0;
+                wb_sel    = 2'b01; // in this case it is don't care because rf_en = 1'b0
+                wr_en     = 1'b0;
+                aluop     = 4'b0000; // aluop is always addition in case of branch instructions
+                br_type   = funct3;
+                br_take   = br_taken;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
             end
             7'b0110111: // U-type (LUI)
             begin
-                rf_en    = 1'b1;
-                sel_a    = 1'b0; //it's actually don't care bcz we don't need opr_a
-                sel_b    = 1'b1;
-                rd_en    = 1'b0;
-                wb_sel   = 2'b01;
-                wr_en    = 1'b0;
-                aluop    = 4'b1010;
-                br_type  = 3'b111;
-                br_take  = 1'b0;
+                rf_en     = 1'b1;
+                sel_a     = 1'b0; //it's actually don't care bcz we don't need opr_a
+                sel_b     = 1'b1;
+                rd_en     = 1'b0;
+                wb_sel    = 2'b01;
+                wr_en     = 1'b0;
+                aluop     = 4'b1010;
+                br_type   = 3'b111;
+                br_take   = 1'b0;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
             end
             7'b0010111: // U-type (AUIPC)
             begin
-                rf_en    = 1'b1;
-                sel_a    = 1'b0;
-                sel_b    = 1'b1;
-                rd_en    = 1'b0;
-                wb_sel   = 2'b01;
-                wr_en    = 1'b0;
-                aluop    = 4'b0000; // ADD
-                br_type  = 3'b111;
-                br_take  = 1'b0;
+                rf_en     = 1'b1;
+                sel_a     = 1'b0;
+                sel_b     = 1'b1;
+                rd_en     = 1'b0;
+                wb_sel    = 2'b01;
+                wr_en     = 1'b0;
+                aluop     = 4'b0000; // ADD
+                br_type   = 3'b111;
+                br_take   = 1'b0;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
             end
             7'b1101111: // J-type (JAL)
             begin
-                rf_en    = 1'b1;
-                sel_a    = 1'b0;
-                sel_b    = 1'b1;
-                rd_en    = 1'b0;
-                wb_sel   = 2'b00;
-                wr_en    = 1'b0;
-                aluop    = 4'b0000; // ADD
-                br_type  = 3'b111;
-                br_take  = 1'b1;
+                rf_en     = 1'b1;
+                sel_a     = 1'b0;
+                sel_b     = 1'b1;
+                rd_en     = 1'b0;
+                wb_sel    = 2'b00;
+                wr_en     = 1'b0;
+                aluop     = 4'b0000; // ADD
+                br_type   = 3'b111;
+                br_take   = 1'b1;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
             end
             7'b1100111: // JALR
             begin
-                rf_en = 1'b1;
-                sel_a = 1'b1;
-                sel_b = 1'b1;
-                rd_en = 1'b0;
-                wb_sel = 2'b00;
-                wr_en = 1'b0;
-                aluop = 4'b0000;
-                br_type = 3'b111;
-                br_take = 1'b1;
+                rf_en     = 1'b1;
+                sel_a     = 1'b1;
+                sel_b     = 1'b1;
+                rd_en     = 1'b0;
+                wb_sel    = 2'b00;
+                wr_en     = 1'b0;
+                aluop     = 4'b0000;
+                br_type   = 3'b111;
+                br_take   = 1'b1;
+                csr_rd    = 1'b0;
+                csr_wr    = 1'b0;
+                is_mret   = 1'b0;
+            end
+            7'b1110011: // CSRRW
+            begin
+                case (funct3)
+                3'b000: // MRET
+                    begin
+                        rf_en        = 1'b0;
+                        sel_a        = 1'b1;
+                        sel_b        = 1'b0;
+                        rd_en        = 1'b0;
+                        wb_sel       = 2'b01;
+                        wr_en        = 1'b0;
+                        br_take      = 1'b0;
+                        mem_acc_mode = 3'b111;
+                        br_type      = 3'b111;
+                        csr_rd       = 1'b0;
+                        csr_wr       = 1'b0;
+                        is_mret      = 1'b1;
+                    end
+
+                default:
+                    begin
+                        rf_en        = 1'b1;
+                        sel_a        = 1'b1;
+                        sel_b        = 1'b0;
+                        rd_en        = 1'b0;
+                        wb_sel       = 2'b11;
+                        wr_en        = 1'b0;
+                        br_take      = 1'b0;
+                        mem_acc_mode = 3'b111;
+                        br_type      = 3'b111;
+                        csr_rd       = 1'b1;
+                        csr_wr       = 1'b1;
+                        is_mret      = 1'b0;
+                    end
+                endcase
             end
             default:
             begin
@@ -209,6 +253,9 @@ module controller
                 br_take      = 1'b0;
                 mem_acc_mode = 3'b111;
                 br_type      = 3'b111;
+                csr_rd       = 1'b0;
+                csr_wr       = 1'b0;
+                is_mret      = 1'b0;
             end
         endcase
     end
--- processor.sv
+++ processor.sv
@@ -4,64 +4,116 @@ module processor
     input logic rst
 ); 
     // wires
+
     logic        rf_en;
-    logic        sel_b;
-    logic [31:0] pc_out;
-    logic [31:0] new_pc;
-    logic [31:0] inst;
+    logic [ 2:0] imm_type;
     logic [ 4:0] rd;
     logic [ 4:0] rs1;
     logic [ 4:0] rs2;
     logic [ 6:0] opcode;
     logic [ 2:0] funct3;
     logic [ 6:0] funct7;
-    logic [31:0] rdata1;
-    logic [31:0] rdata2;
-    logic [31:0] opr_a;
-    logic [31:0] opr_b;
-    logic [31:0] opr_res;
-    logic [11:0] imm;
-    logic [31:0] imm_val;
-    logic [31:0] wdata;
-    logic [3 :0] aluop;
-    logic [31:0] rdata;
-    logic        rd_en;
-    logic        wr_en;
-    logic [ 1:0] wb_sel;
-    logic        br_taken;
-    logic [ 2:0] br_type;
-    logic [ 2:0] mem_acc_mode;
 
+    logic [31:0] mux_out_pc;
+    logic [31:0] mux_out_opr_a;
+    logic [31:0] mux_out_opr_b;
 
-    // PC MUX
-    mux_2x1 mux_2x1_pc
-    (
-        .in_0        ( pc_out + 32'd4 ),
-        .in_1        ( opr_res        ),
-        .select_line ( br_taken       ),
-        .out         ( new_pc         )
-    );
+    logic        dm_en_id;
+    logic        dm_en_ex;
+    logic        dm_en_mem;
+
+    logic        sel_pc_if;
+    logic        sel_pc_id;
+
+    logic        sel_opr_a_id;
+    logic        sel_opr_a_ex;
+
+    logic        sel_opr_b_id;
+    logic        sel_opr_b_ex;
+
+    logic [ 1:0] sel_wb_id;
+    logic [ 1:0] sel_wb_ex;
+    logic [ 1:0] sel_wb_mem;
+    logic [ 1:0] sel_wb_wb;
+
+    logic [31:0] pc_out_if;
+    logic [31:0] pc_out_id;
+    logic [31:0] pc_out_ex;
+    logic [31:0] pc_out_mem;
+    logic [31:0] pc_out_wb;
+
+    logic [31:0] inst_if;
+    logic [31:0] inst_id;
+    
+    logic [ 3:0] aluop_id;
+    logic [ 3:0] aluop_ex;
+
+    logic [31:0] wdata_wb;
+    logic [31:0] wdata_id;
+
+    logic [31:0] rdata1_id;
+    logic [31:0] rdata1_ex;
+
+    logic [31:0] rdata2_id;
+    logic [31:0] rdata2_ex;
+    logic [31:0] rdata2_mem;
+    
+    logic [31:0] imm_id;
+    logic [31:0] imm_ex;
+
+    logic [31:0] opr_res_if;
+    logic [31:0] opr_res_ex;
+    logic [31:0] opr_res_mem;
+    logic [31:0] opr_res_wb;
+
+    logic [31:0] dmem_out_mem;
+    logic [31:0] dmem_out_wb;
+
+    // --------------------- Instruction Fetch ---------------------
+
+    // pc selection mux
+    assign mux_out_pc = sel_pc_if ? opr_res_if : (pc_out_if + 32'd4);
 
     // program counter
     pc pc_i
     (
         .clk   ( clk            ),
         .rst   ( rst            ),
-        .pc_in ( new_pc         ),
-        .pc_out( pc_out         )
+        .pc_in ( mux_out_pc     ),
+        .pc_out( pc_out_if      )
     );
 
     // instruction memory
     inst_mem inst_mem_i
     (
-        .addr  ( pc_out         ),
-        .data  ( inst           )
+        .addr  ( pc_out_if      ),
+        .data  ( inst_if        )
     );
+
+    // -------------------------------------------------------------
+
+
+    // IF <-> ID
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            inst_id   <= 0;
+            pc_out_id <= 0;
+        end
+        else
+        begin
+            inst_id   <= inst_if;
+            pc_out_id <= pc_out_if;
+        end
+    end
+
+    // --------------------- Instruction Decode ---------------------
 
     // instruction decoder
     inst_dec inst_dec_i
     (
-        .inst  ( inst           ),
+        .inst  ( inst_id        ),
         .rs1   ( rs1            ),
         .rs2   ( rs2            ),
         .rd    ( rd             ),
@@ -78,89 +130,180 @@ module processor
         .rd    ( rd             ),
         .rs1   ( rs1            ),
         .rs2   ( rs2            ),
-        .rdata1( rdata1         ),
-        .rdata2( rdata2         ),
-        .wdata ( wdata          )
+        .rdata1( rdata1_id      ),
+        .rdata2( rdata2_id      ),
+        .wdata ( wdata_id       )
+    );
+
+    // controller
+    controller controller_i
+    (
+        .opcode    ( opcode       ),
+        .funct3    ( funct3       ),
+        .funct7    ( funct7       ),
+        .rf_en     ( rf_en_id     ),
+        .dm_en     ( dm_en_id     ),
+        .sel_opr_a ( sel_opr_a_id ),
+        .sel_opr_b ( sel_opr_b_id ),
+        .sel_pc    ( sel_pc_id    ),
+        .sel_wb    ( sel_wb_id    ),
+        .imm_type  ( imm_type     ),
+        .aluop     ( aluop_id     )
     );
 
     // immediate generator
     imm_gen imm_gen_i
     (
-        .inst   ( inst          ),
-        .imm_val( imm_val       )
+        .inst      ( inst_id        ),
+        .imm_type  ( imm_type       ),
+        .imm       ( imm_id         )
     );
 
-    // ALU opr_a MUX
+    // -------------------------------------------------------------
 
-    mux_2x1 mux_2x1_alu_opr_a
-    (
-        .in_0           ( pc_out  ),
-        .in_1           ( rdata1  ),
-        .select_line    ( sel_a   ),
-        .out            ( opr_a   )
-    );
 
-    // ALU opr_b MUX
-    mux_2x1 mux_2x1_alu_opr_b
-    (
-        .in_0           ( rdata2  ),
-        .in_1           ( imm_val ),
-        .select_line    ( sel_b   ),
-        .out            ( opr_b   )
-    );
+    // ID <-> EX
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            pc_out_ex    <= 0;
+            rdata1_ex    <= 0;
+            rdata2_ex    <= 0;
+            imm_ex       <= 0;
 
+            // control signals
+            dm_en_ex     <= 0;
+            sel_opr_a_ex <= 0;
+            sel_opr_b_ex <= 0;
+            sel_wb_ex    <= 0;
+            aluop_ex     <= 0;
+
+        end
+        else
+        begin
+            pc_out_ex    <= pc_out_id;
+            rdata1_ex    <= rdata1_id;
+            rdata2_ex    <= rdata2_id;
+            imm_ex       <= imm_id;
+            
+            // control signals
+            dm_en_ex     <= dm_en_id;
+            sel_opr_a_ex <= sel_opr_a_id;
+            sel_opr_b_ex <= sel_opr_b_id;
+            sel_wb_ex    <= sel_wb_id;
+            aluop_ex     <= aluop_id;
+        end
+    end
+
+    // Feedback from ID to IF
+    always_comb
+    begin
+        sel_pc_if = sel_pc_id;
+    end
+
+    // --------------------- Execute ---------------------
+
+    // operand a selection mux
+    assign mux_out_opr_a = sel_opr_a_ex ? pc_out_ex : rdata1_ex;
+
+    // operand b selection mux
+    assign mux_out_opr_b = sel_opr_b_ex ?    imm_ex : rdata2_ex;
 
     // alu
     alu alu_i
     (
-        .aluop   ( aluop          ),
-        .opr_a   ( opr_a          ),
-        .opr_b   ( opr_b          ),
-        .opr_res ( opr_res        )
+        .aluop   ( aluop_ex       ),
+        .opr_a   ( mux_out_opr_a  ),
+        .opr_b   ( mux_out_opr_b  ),
+        .opr_res ( opr_res_ex     )
     );
 
+    // -------------------------------------------------------------
 
+    // EX <-> MEM
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            pc_out_mem  <= 0;
+            opr_res_mem <= 0;
+            rdata2_mem  <= 0;
+
+            // control signals
+            dm_en_mem   <= 0;
+            sel_wb_mem  <= 0;
+        end
+        else
+        begin
+            pc_out_mem  <= pc_out_ex;
+            opr_res_mem <= opr_res_ex;
+            rdata2_mem  <= rdata2_ex;
+
+            // control signals
+            dm_en_mem   <= dm_en_ex;
+            sel_wb_mem  <= sel_wb_ex;
+        end
+    end
+
+    // ----------------------- Memory ------------------------
+
+    // data memory
     data_mem data_mem_i
     (
-        .clk            ( clk          ),
-        .rd_en          ( rd_en        ),
-        .wr_en          ( wr_en        ),
-        .addr           ( opr_res      ),
-        .mem_acc_mode   ( mem_acc_mode ),
-        .rdata2         ( rdata2       ),
-        .rdata          ( rdata        )
+        .clk      ( clk           ),
+        .dm_en    ( dm_en_mem     ),
+        .addr     ( opr_res_mem   ),
+        .data_in  ( rdata2_mem    ),
+        .data_out ( dmem_out_mem  )
     );
 
+    // ---------------------------------------------------------
 
-    // Writeback MUX
-    mux_3x1 wb_mux
-    (
-        .in_0           ( pc_out + 32'd4 ),
-        .in_1           ( opr_res        ),
-        .in_2           ( rdata          ),
-        .select_line    ( wb_sel         ),
-        .out            ( wdata          )
-    );
+    // MEM <-> WB
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            pc_out_wb   <= 0;
+            opr_res_wb  <= 0;
+            dmem_out_wb <= 0;
 
+            sel_wb_wb   <= 0;
+        end
+        else
+        begin
+            pc_out_wb   <= pc_out_mem;
+            opr_res_wb  <= opr_res_mem;
+            dmem_out_wb <= dmem_out_mem;
+            sel_wb_wb   <= sel_wb_mem;
+        end
+    end
 
-    // controller
-    controller controller_i
-    (
-        .opcode         ( opcode         ),
-        .funct3         ( funct3         ),
-        .funct7         ( funct7         ),
-        .br_taken       ( br_taken       ),
-        .aluop          ( aluop          ),
-        .rf_en          ( rf_en          ),
-        .sel_a          ( sel_a          ),
-        .sel_b          ( sel_b          ),
-        .rd_en          ( rd_en          ),
-        .wr_en          ( wr_en          ),
-        .wb_sel         ( wb_sel         ),
-        .mem_acc_mode   ( mem_acc_mode   ),
-        .br_type        ( br_type        ),
-        .br_take        ( br_taken       )
-    );
+    always_comb
+    begin
+        opr_res_if = opr_res_mem;
+    end
 
+    // --------------------- Write Back ---------------------
+
+    // write back selection mux
+    always_comb
+    begin
+        case(sel_wb_wb)
+            2'b00: wdata_wb = opr_res_wb;
+            2'b01: wdata_wb = dmem_out_wb;
+            2'b10: wdata_wb = pc_out_wb + 32'd4;
+            2'b11: wdata_wb = 32'd0;
+        endcase
+    end
+
+    // -------------------------------------------------------------
+
+    // Feedback from WB to ID
+    always_comb
+    begin
+        wdata_id = wdata_wb;
+    end
     
 endmodule
